bbfc4a54a6debe2ea420ae43b958d85643ff5b79
Senthil Nathan <email.senthil.nathan@gmail.com>
2020-12-11 11:16:47 +0800

    First commit for openshift by senthil

---
+++ src/test/java/com/senthil/spring/boot/rest/api/springbootrestapi100/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senthil.spring.boot.rest.api.springbootrestapi100;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
